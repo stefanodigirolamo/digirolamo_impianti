24c41762e9f2c4eaa5801a14917ffeadb3aaaed7
stefanodigirolamo.apio <stefanodigirolamo@live.it>
2026-06-19 17:35:16 +0200

    Feat: refined robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://tuodominio.it/sitemap.xml",
+    sitemap: "https://digirolamo-impianti.com/sitemap.xml",
   };
 }
